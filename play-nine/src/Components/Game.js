277b2914216fad2bdf4f5b8771849b9de88bdbcd
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2017-07-14 01:25:09 -0700

    success fxn

--- play-nine/src/Components/Game.js
+++ play-nine/src/Components/Game.js
@@ -20,19 +20,19 @@ const Button = (props) => {
 
 		switch (props.answerIsCorrect) {
 			case true:
-					result = <button className="btn btn-lg btn-success"
+					result = <button className="btn btn-font btn-lg btn-success text-center"
 													 onClick={ ()=> props.confirmAnswer() }>
 									<i className="fa fa-check"></i>
 								</button>
 								break;
 			case false:
-					result = <button className="btn btn-lg btn-danger"
+					result = <button className="btn btn-font btn-lg btn-danger text-center"
 													 onClick={ ()=> props.checkAnswer() }>
 										  <i className="fa fa-times"></i>
 										</button>
 								break;
 			default:
-					result = <button className="btn btn-lg btn-info"
+					result = <button className="btn btn-font btn-lg btn-info text-center"
 													disabled={props.selectedNumbers.length === 0}
 													onClick={ ()=> props.checkAnswer() } >=
 								</button>
@@ -41,7 +41,7 @@ const Button = (props) => {
 	return (
 		<div className="col-2">
 			{result}
-			<button className="btn btn-warning btn-lg"
+			<button className="btn btn-font btn-warning btn-lg text-center"
 							onClick={ ()=> props.hitRefresh() }
 							disabled={props.refresh === 0}>
 				<i className="fa fa-refresh"></i> {props.refresh}
@@ -74,17 +74,25 @@ const Numbers = (props) => {
 		}
 	}
 
-
 	const numbers = _.range(1,10).map( (num,i)=>
 		<span key={i}
 					className={numberClassName(num)}
 					onClick={ ()=> props.selectNumber(num)}
 					>{num}</span>
 	)
-
 	return (
 		<div>
 			{numbers}
+		</div>
+	)
+}
+
+const DoneFrame = (props) => {
+	return (
+		<div className="text-center">
+			<h1 className="success">
+				<i className="fa fa-star"></i> {props.doneStatus} <i className="fa fa-star"></i>
+			</h1>
 		</div>
 	)
 }
@@ -97,7 +105,8 @@ class Game extends Component {
 		selectedNumbers: [],
 		usedNumbers: [],
 		answerIsCorrect: null,
-		refresh: 5
+		refresh: 5,
+		doneStatus: null
 	};
 
 	selectNumber(num) {
@@ -128,7 +137,7 @@ class Game extends Component {
 			selectedNumbers: [],
 			usedNumbers: prevState.usedNumbers.concat(prevState.selectedNumbers),
 			answerIsCorrect: null
-		}))
+		}), this.finalAnswer)
 	}
 
 	hitRefresh(){
@@ -141,36 +150,61 @@ class Game extends Component {
 		}))
 	}
 
+	finalAnswer = () => {
+		if(this.state.usedNumbers.length > 8) {
+			this.setState({doneStatus:'SUCCESS YOU DID IT BOYO!'})
+		}
+	}
+
+	reset() {
+		this.setState({
+			randomNumberOfStars: Game.randomNumber(),
+			selectedNumbers: [],
+			usedNumbers: [],
+			answerIsCorrect: null,
+			refresh: 5,
+			doneStatus: null
+		})
+	}
+
 	render() {
 		const {
 			randomNumberOfStars,
 			selectedNumbers,
 			answerIsCorrect,
 			usedNumbers,
-			refresh } = this.state;
+			refresh,
+		 	doneStatus} = this.state;
 		return (
 			<div className="container">
-				<h1>Play Nine</h1>
+				<h1 className="title">Play Nine</h1>
 				<hr />
 				<div className="row">
 					<Stars numberOfStars={randomNumberOfStars} />
+
 					<Button selectedNumbers={selectedNumbers}
 									checkAnswer={this.checkAnswer.bind(this)}
 									answerIsCorrect={answerIsCorrect}
 									confirmAnswer={this.confirmAnswer.bind(this)}
 									refresh={refresh}
-									hitRefresh={this.hitRefresh.bind(this)}
-									/>
+									hitRefresh={this.hitRefresh.bind(this)} />
+
 					<Answer unselectNumber={this.unselectNumber.bind(this)}
-									selectedNumbers={selectedNumbers}
-									/>
+									selectedNumbers={selectedNumbers} />
 				</div>
 				<br />
-				<div className="card text-center">
-					<Numbers selectNumber={this.selectNumber.bind(this)}
-										selectedNumbers={selectedNumbers}
-										usedNumbers={usedNumbers}/>
-				</div>
+
+				{doneStatus ? <DoneFrame doneStatus={doneStatus}/>
+					:
+					<div className="card text-center">
+						<Numbers selectNumber={this.selectNumber.bind(this)}
+											selectedNumbers={selectedNumbers}
+											usedNumbers={usedNumbers}/>
+					</div>
+				}
+				<button className="btn btn-primary btn-lg"
+								onClick={ ()=> this.reset() }>Reset</button>
+
 			</div>
 		)
 	}
